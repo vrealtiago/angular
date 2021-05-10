d62967b8285490ed0cf3a2d2a4141cd2db02924b
Tiago Real <vrealtiago@gmail.com>
2021-05-11 01:08:52 +0200

    Add Lab Forward project

---
+++ lab-notebook-app/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'lab-notebook-app';
+  selectedWord = '';
+  qtyWords = 0;
+  similarWordList = '';
+  selectedText = 'Word Words Wor word fom kok pip Word loko mesmo wwooww worlock wokaka\n' +
+    '\n' +
+    'Word Word Word word';
+  resultBody;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.resultBody = document.getElementById('resultContent');
+    }
+
+  checkWord(): void {
+    this.cleanSearch();
+    if (this.selectedWord !== '' || this.selectedText === '') {
+      this.findSelectedWords();
+    }
+  }
+
+  findSelectedWords(): any {
+    const txt = this.selectedText.split(/\s|\\n/g);
+    const similarList = [];
+    const partOfWord = this.selectedWord.substring(0, this.selectedWord.length > 3 ? 3 : this.selectedWord.length / 2);
+    const similarRegex = new RegExp('((?<!\\w)' + partOfWord + '\\w+)', 'gi');
+    txt.forEach((item, index) => {
+      if (item == this.selectedWord) {
+        txt[index] = '<mark>' + item + '</mark>';
+        this.qtyWords = this.qtyWords + 1;
+      } else if (item.match(similarRegex)) {
+        // console.log(item.match(similarRegex));
+        similarList.push(item);
+        txt[index] = '<span class="similarWord">' + item + '</span>';
+      } else if (item == '') {
+        txt[index] = '<br /><br />';
+      }
+    });
+    this.similarWordList = similarList.join(', ');
+    // console.log(this.similarWordList);
+    this.resultBody.innerHTML = txt.join(' ');
+  }
+
+  cleanSearch(): void  {
+    this.resultBody.innerHTML = '';
+    this.qtyWords = 0;
+    this.similarWordList = '';
+  }
+}
